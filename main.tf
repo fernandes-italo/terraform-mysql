2e0975f3f5f90d5f00bb633827005af37f8ba015
Italo Fernandes <italo.feernandes@gmail.com>
2021-05-08 17:39:13 -0300

    create storage account

--- main.tf
+++ main.tf
@@ -37,56 +37,64 @@ resource "azurerm_subnet" "subnet" {
 }
 
 resource "azurerm_public_ip" "publicIp" {
-    name                         = "publicIP"
-    location                     = azurerm_resource_group.rg.location
-    resource_group_name          = azurerm_resource_group.rg.name
-    allocation_method            = "Static"
+  name                = "publicIP"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
 }
 
 resource "azurerm_network_security_group" "securityGroup" {
-    name                = "securityGroup"
-    location            = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.rg.name
+  name                = "securityGroup"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
-    security_rule {
-        name                       = "SSH"
-        priority                   = 1000
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "22"
-        source_address_prefix      = "*"
-        destination_address_prefix = "*"
-    }
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 
-    security_rule {
-        name                       = "MYSQL"
-        priority                   = 1001
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "3306"
-        source_address_prefix      = "*"
-        destination_address_prefix = "*"
-    }
+  security_rule {
+    name                       = "MYSQL"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3306"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_network_interface" "networkInterface" {
-    name                      = "networkInterface"
-    location                  = azurerm_resource_group.rg.location
-    resource_group_name       = azurerm_resource_group.rg.name
+  name                = "networkInterface"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
-    ip_configuration {
-        name                          = "niConfiguration"
-        subnet_id                     = azurerm_subnet.subnet.id
-        private_ip_address_allocation = "Dynamic"
-        public_ip_address_id          = azurerm_public_ip.publicIp.id
-    }
+  ip_configuration {
+    name                          = "niConfiguration"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.publicIp.id
+  }
 }
 
 resource "azurerm_network_interface_security_group_association" "nisga" {
-    network_interface_id      = azurerm_network_interface.networkInterface.id
-    network_security_group_id = azurerm_network_security_group.securityGroup.id
+  network_interface_id      = azurerm_network_interface.networkInterface.id
+  network_security_group_id = azurerm_network_security_group.securityGroup.id
+}
+
+resource "azurerm_storage_account" "storagesamysql" {
+  name                     = "storagesamysql"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
