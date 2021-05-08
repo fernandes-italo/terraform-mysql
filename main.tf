db876f77209c304731d0a5715e5e85a2bbb8cc9c
Italo Fernandes <italo.feernandes@gmail.com>
2021-05-08 16:52:35 -0300

    create subnet

--- main.tf
+++ main.tf
@@ -25,6 +25,13 @@ resource "azurerm_resource_group" "rg" {
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = "eastus"
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
