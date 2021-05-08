f4d6e41d4551496f624a2b5e1c0362e2b94eb1d4
Italo Fernandes <italo.feernandes@gmail.com>
2021-05-08 16:36:51 -0300

    rename resource group and create virtual ntework

--- main.tf
+++ main.tf
@@ -14,10 +14,17 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg-atividade2" {
-  name     = "rg-atividade2"
+resource "azurerm_resource_group" "rg" {
+  name     = "rg"
   location = "eastus"
   tags = {
-    "Environment" = "atividade 1"
+    "Environment" = "atividade 2"
   }
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "eastus"
+  resource_group_name = azurerm_resource_group.rg.name
 }
