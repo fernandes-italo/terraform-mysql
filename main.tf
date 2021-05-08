dd1fdb5e97f628e302af8b5d02777befe9f62f55
Italo Fernandes <italo.feernandes@gmail.com>
2021-05-08 16:55:22 -0300

    create public IP

--- main.tf
+++ main.tf
@@ -35,3 +35,10 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+
+resource "azurerm_public_ip" "publicIp" {
+    name                         = "publicIP"
+    location                     = azurerm_resource_group.rg.location
+    resource_group_name          = azurerm_resource_group.rg.name
+    allocation_method            = "Static"
+}
